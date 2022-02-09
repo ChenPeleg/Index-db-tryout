3bc1dd6d85c775202e79e70a1cdc2a43ab316bae
Chen <Chenpeleg>
2022-02-09 10:19:50 +0200

    added webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+const path = require('path')
 module.exports = {
+    entry: "./src/app.ts",
+    output: {
+        filename: "bundle.app.js",
+        path: path.resolve(__dirname, 'assets', 'js')
 
+    }
 }
