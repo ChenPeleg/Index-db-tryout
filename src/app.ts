ba63e5aa2f24bb24a92ea89dd6c694f76713f64c
Chen <Chenpeleg>
2022-02-22 12:22:36 +0200

    display message

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,19 @@
-import randomImoji from './services/randomImoji';
+import { DomRender } from './services/dom-render';
+
 import './style/style.main.scss'
 
-class DOMRender {
+class App {
     constructor() {
 
     }
-    public alertMessage(m: string) {
-        this.#alertMessage(m)
-    }
-    #render() {
-        let s: string = 'asdf';
-
+    start() {
+        const renderer = new DomRender();
+        renderer.Message('App started 2')
 
     }
-    #alertMessage(m: string) {
-        const div: Element | null = document.querySelector('#main-div-id');
-        console.log(div)
-        div ? div.innerHTML = 'this works 4' + this.#getRand() : null;
 
-
-        //  alert(m)
-
-    }
-    #getRand(): string {
-        return '<br><br>' + randomImoji();
-    }
 
 }
 
-const rendere: DOMRender = new DOMRender();
-rendere.alertMessage('alert message');
+const app: App = new App();
+app.start();
